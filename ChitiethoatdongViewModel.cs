7089158a8fcf68379c04e2c4913fee8c66a69555
Hà My <145450102+hami308@users.noreply.github.com>
2024-05-17 07:22:53 +0700

    Add files via upload

---
+++ ChitiethoatdongViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace PBL3_QLHDTN.ViewModel
+{
+    public class ChitiethoatdongViewModel
+    {
+        public int Idhoatdong { get; set; }
+        public string? Tenhoatdong { get; set; }
+        public DateTime? Thoigianbatdau { get; set; }
+        public DateTime? Thoigiaketthuc { get; set; }
+        public DateTime? Tgbdchinhsua { get; set; }
+        public DateTime? Tgktchinhsua { get; set; }
+        public string? Linhvuc { get; set; }
+         public string Tinhtrang {  get; set; } 
+        public DateTime? Thoigianhuy { get; set; }
+        public string? Lydohuy { get; set; }
+        public string? DiaDiem { get; set; }
+        public string? MuctieuHd { get; set; }
+        public string? Danhgiatutochuc { get; set; }
+        public DateTime? Tgdanhgia1 { get; set; }
+        public string? Danhgiacuatnv { get; set; }
+        public DateTime? Tgdanhgia2  { get; set; }
+    }
+}
---
+++ HoatdongcanhanViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PBL3_QLHDTN.Models;
+
+namespace PBL3_QLHDTN.ViewModel
+{
+    public class HoatdongcanhanViewModel
+    {
+        public int? Idhoatdong { get; set; }
+        public int? Idtochuc { get; set; }
+        public string? Tenhoatdong { get; set; }
+        public DateTime? Thoigiandangky { get; set; }
+        public DateTime? Thoigianbatdau { get; set; }
+        public DateTime? Thoigianketthuc { get; set; }
+        public string ? Tinhtrang { get; set; }
+
+    }
+}
